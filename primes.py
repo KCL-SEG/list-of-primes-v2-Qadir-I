ea0a99e4b17c19c0b8b22a5c163bf22a7bbf79b9
Qadir Ijaz Fernandez <qadir.ijaz01@gmail.com>
2022-10-23 16:39:59 +0100

    Implement solution for list of primes

--- primes.py
+++ primes.py
@@ -3,4 +3,20 @@
 
 def primes(number_of_primes):
     list = []
+    number = 2
+
+    if number_of_primes <= 0:
+        raise ValueError
+    
+    while len(list) < number_of_primes:
+        prime = True
+        
+        for i in range(2, number):
+            if number % i == 0:
+                prime = False
+        
+        if prime:
+            list.append(number)
+        number += 1
+
     return list
